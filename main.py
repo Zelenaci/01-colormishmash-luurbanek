3f38391476ead21001f792ec74ed95f65e6f0e07
Lukáš Urbánek <93544030+luurbanek@users.noreply.github.com>
2021-12-13 14:16:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Label, Button, Scale, HORIZONTAL, Canvas
+from tkinter import ( Label, Button, Scale, HORIZONTAL, Canvas, Frame, Entry, LEFT, S, StringVar, )
 
 # from tkinter import ttk
 
@@ -11,10 +11,8 @@ from tkinter import Label, Button, Scale, HORIZONTAL, Canvas
     def __init__(self, parent):
         super().__init__(parent, class_=parent.name)
         self.config()
-
         btn = tk.Button(self, text="Konec", command=self.close)
         btn.pack()
-
     def close(self):
         self.destroy() """ # trida About pro viceoknove aplikace
 
@@ -29,22 +27,49 @@ class Application(tk.Tk): #dědí z tk.Tk
 
         self.bind("<Escape>", self.quit) #= kdyz zmacknu esc tak ukonci
 
-        self.lblR = tk.Label(self, text="R")
-        self.lblR.pack() #umisteni widgetu do programu
-        self.scaleR = Scale (from_=0, to=255, orient=HORIZONTAL,length=256, command=self.change)
-        self.scaleR.pack()
+        ###  R
+        self.varR = StringVar()
+        self.varR.trace("w",self)
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.lblR = Label(self.frameR, text="R")
+        self.lblR.pack(side=LEFT, anchor=S) #umisteni widgetu do programu
+        self.scaleR = Scale (self.frameR, 
+            from_=0, to=255, orient=HORIZONTAL,length=256, variable=self.varR, command=self.change,
+        )
+        self.scaleR.pack(side=LEFT, anchor=S)
+        self.entryR = Entry(self.frameR, width=5, textvariable=self.varR)
+        self.entryR.pack(side=LEFT, anchor=S)
 
-        self.lblG = tk.Label(self, text="G")
-        self.lblG.pack() #umisteni widgetu do programu
-        self.scaleG = Scale (from_=0, to=255, orient=HORIZONTAL,length=256, command=self.change)
-        self.scaleG.pack()
+        ###  G
+        self.varG = StringVar()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.lblG = Label(self.frameG, text="G")
+        self.lblG.pack(side=LEFT, anchor=S) #umisteni widgetu do programu
+        self.scaleG = Scale (self.frameG, 
+            from_=0, to=255, orient=HORIZONTAL,length=256, variable=self.varG, command=self.change,
+        )
+        self.scaleG.pack(side=LEFT, anchor=S)
+        self.entryG = Entry(self.frameG, width=5, textvariable=self.varG)
+        self.entryG.pack(side=LEFT, anchor=S)
 
-        self.lblB = tk.Label(self, text="B")
-        self.lblB.pack() #umisteni widgetu do programu
-        self.scaleB = Scale (from_=0, to=255, orient=HORIZONTAL,length=256, command=self.change)
-        self.scaleB.pack()
 
-        self.canvasMain = Canvas(width=256, height=100, background="#123456")
+        ###  B
+        self.varB = StringVar()
+        self.frameB = Frame(self)
+        self.frameB.pack()
+        self.lblB = Label(self.frameB, text="B")
+        self.lblB.pack(side=LEFT, anchor=S) #umisteni widgetu do programu
+        self.scaleB = Scale (self.frameB, 
+            from_=0, to=255, orient=HORIZONTAL,length=256, variable=self.varB, command=self.change,
+        )
+        self.scaleB.pack(side=LEFT, anchor=S)
+        self.entryB = Entry(self.frameB, width=5, textvariable=self.varB)
+        self.entryB.pack(side=LEFT, anchor=S)
+
+
+        self.canvasMain = Canvas(width=256, height=100, background="#000000")
         self.canvasMain.pack()
 
 
@@ -58,14 +83,20 @@ class Application(tk.Tk): #dědí z tk.Tk
         window.grab_set()"""
 
     def change(self, event):
-        self.lblG.config(text="changeconfigtext")
+        #self.lblG.config(text="changeconfigtext")
 
         r = self.scaleR.get()
         g = self.scaleG.get()
         b = self.scaleB.get()
-        self.canvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
-        print(f"#{r:2x}{g:2x}{b:2x}")
-        print(f"#{r:02x}{g:02x}{b:02x}")
+        colorstring = f"#{r:02X}{g:02X}{b:02X}"
+        self.canvasMain.config(background=colorstring)
+        print(colorstring)
+        #print(f"#{r:2x}{g:2x}{b:2x}")
+        #print(f"#{r:02x}{g:02x}{b:02x}")
+
+        self.varR.set(r)
+        self.varG.set(g)
+        self.varB.set(b)
 
 
     def quit(self, event=None):
@@ -74,4 +105,3 @@ class Application(tk.Tk): #dědí z tk.Tk
 
 app = Application()
 app.mainloop()
-
