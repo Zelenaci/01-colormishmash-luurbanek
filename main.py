2d449ff0c6aa913ef432b39b18ad69da40dc5cd3
Lukáš Urbánek <93544030+luurbanek@users.noreply.github.com>
2022-01-10 14:17:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Application(tk.Tk):  # dědí z tk.Tk
         self.title(self.name)
 
         self.bind("<Escape>", self.quit)  # = kdyz zmacknu esc tak ukonci
+        self.protocol("WM_DELETE_WINDOW", self.quit)
 
         ###  R
         self.varR = IntVar()
@@ -90,22 +91,24 @@ class Application(tk.Tk):  # dědí z tk.Tk
         self.frameMem.pack()
         self.canvasMem = []
 
-        for row in range(3):
-            for column in range(7):
-                canvas = Canvas(self.frameMem, width=50, height=50, background="#FFFFFF")
-                canvas.grid(row=row, column=column)
+        for row in range(10):
+            for column in range(10):
+                canvas = Canvas(self.frameMem, width=50, height=50, background="#000000") #vytvorim canvas
+                canvas.grid(row=row, column=column) #pridam ho do mrizky
                 canvas.bind("<Button-1>", self.mousehandler)
                 self.canvasMem.append(canvas)
 
     def mousehandler(self, event):
         # print(dir(event))
-        if self.cget("cursor") != "pencil":
+        if self.cget("cursor") != "pencil": #kliknu poprve
             self.config(cursor="pencil")
             self.color = event.widget.cget("background")
-
-        elif self.cget("cursor") == "pencil":
+        elif self.cget("cursor") == "pencil": # druhy klik
             self.config(cursor="")
             event.widget.config(background=self.color)
+
+            if event.widget is self.canvasMain:
+                self.canvasColor2Slides(self.canvasMain)
 
     def change(self, var, index, mode):
 
@@ -120,8 +123,34 @@ class Application(tk.Tk):  # dědí z tk.Tk
         self.varG.set(g)
         self.varB.set(b)
 
+    def canvasColor2Slides(self, canvas):
+        color = canvas.cget("background") #ziskam barvu cget
+        r = int(color[1:3], 16)
+        g = int(color[3:5], 16)
+        b = int(color[5:], 16)  # ,16 = sestnactkova soustava
+        self.varR.set(r)
+        self.varG.set(g)
+        self.varB.set(b)
+        self.change()
+
+    def colorSave(self):
+        with open("colors.txt", "w") as f:
+            f.write(self.canvasMain.cget("background")+ "\n") #cget config get ... get background
+            for canvas in self.canvasMem:
+                f.write(canvas.cget("background") + "\n")
+
+    def colorLoad(self):
+        if exists ("colors.txt"):
+            with open("colors.txt", "r") as f:
+                colorcode = f.readline().strip() #strip odstrani konec radku
+                self.canvasMain.config(background=colorcode)
+                    canvas.config(background=colorcode)
+        else:
+            print("nepodarilo se nacist soubor")
+
     def quit(self, event=None):
-        super().quit()
+        self.colorSave()
+        super().quit() #super ukazuje na predka ktereho ma pak quit zavolat
 
 
 app = Application()
